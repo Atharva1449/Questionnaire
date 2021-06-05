d632116b59f574aeeebb0285c79cb93fb47a2cd9
atharva_dawande <atharva.dawande603@gmail.com>
2021-06-05 13:39:13 +0530

    nav bar setup

---
+++ main1.js
@@ -0,0 +1,18 @@
+if(window.FileReader && Modernizr.draganddrop){
+
+	function allowDrop(ev){
+		ev.preventDefault();
+	}
+
+	function drag(ev){
+		ev.dataTransfer.setData("Text",ev.target.id);
+	}
+
+	function drop(ev){
+		ev.preventDefault();
+		var data=ev.dataTransfer.getData("Text");
+		ev.target.appendChild(document.getElementById(data));
+	}
+} else {
+	document.write('Sorry, your browser does not support drag and drop');
+}
